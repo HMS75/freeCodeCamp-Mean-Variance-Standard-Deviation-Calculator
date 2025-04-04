f606e268a86a95ab279b99ad7a92cfec1d89cd42
HMS75 <145380884+HMS75@users.noreply.github.com>
2025-04-04 20:14:59 +0530

    Create mean_var_std.py

---
+++ mean_var_std.py
@@ -0,0 +1,14 @@
+import numpy as np
+def calculate(lst):
+    arr = np.array(lst).reshape((3,3))
+    res = {
+        #find "statistic" of axis1, axis2, and flatten it.
+        'mean': [np.mean(arr, axis=0).tolist(), np.mean(arr, axis=1).tolist(), np.mean(arr).tolist()],
+        'var': [np.var(arr, axis=0).tolist(), np.var(arr, axis=1).tolist(), np.var(arr).tolist()],
+        'std': [np.std(arr, axis=0).tolist(), np.std(arr, axis=1).tolist(), np.std(arr).tolist()],
+        'max': [np.max(arr, axis=0).tolist(), np.max(arr, axis=1).tolist(), np.max(arr).tolist()],
+        'min': [np.min(arr, axis=0).tolist(), np.min(arr, axis=1).tolist(), np.min(arr).tolist()],
+        'sum': [np.sum(arr, axis=0).tolist(), np.sum(arr, axis=1).tolist(), np.sum(arr).tolist()]
+    }
+    #return dictionary of required result
+    return res
